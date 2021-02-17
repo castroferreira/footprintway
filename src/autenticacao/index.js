b8915e878c676f71b941f12bdd98a71a85077333
Andreia Ferreira <castroferreira@ua.pt>
2021-02-17 13:31:11 +0000

    tentativa auth

---
+++ src/autenticacao/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const axios = require('axios')
+
+const clientID = 'lPgA7jjLSDfDy0rDnh2kgPsUdiwa'
+const clientSecret = 'aNJfICP7fgr1cRt5JfBfGHrbTd4a'
+
+const app = express()
+app.use(express.static(`./public`)) //${__dirname}
+
+app.get('/oauth', (req, res) => {
+    const requestToken = req.query.code
+    let accessToken;
+
+    const buff = Buffer.from(clientID + ':' + clientSecret);
+    const base64data = buff.toString('base64');
+
+    axios({
+        url: `https://wso2-gw.ua.pt/token?grant_type=authorization_code&code=${requestToken}&schema=openid&redirect_uri=localhost:3000`,
+        method: 'POST',
+        headers: {
+            Authorization: `Basic ${base64data}`,
+            accept: 'application/json'
+        }
+    }).then(response => {
+        accessToken = response.data.access_token;
+
+        return axios({
+            url: 'https://wso2-gw.ua.pt/userinfo?schema=openid',
+            headers: { Authorization: `Bearer ${accessToken}` }
+        });
+    }).then(response => {
+        console.warn("User: ", response.data);
+        console.warn("Access Token: ", accessToken);
+
+        /**
+         * Agora precisam de guardar o access token e o user info algures
+         *
+         * O user precisa de enviar sempre o access token, e se quiserem validar que ele está logged
+         * precisam de perguntar pelo user info outra vez. Se falhar, têm de expirar o login
+         */
+
+        res.redirect('index.html');
+    }).catch(console.error)
+})
+
+app.listen(3000)
