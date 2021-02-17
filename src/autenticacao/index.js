524e0dff56c2fc15db163763ef21667fcc1835b0
Andreia Ferreira <castroferreira@ua.pt>
2021-02-17 16:28:05 +0000

    erro id client - a corrigir

--- src/autenticacao/index.js
+++ src/autenticacao/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const axios = require('axios')
+const cookieParser = require('cookie-parser')
 
-const clientID = 'lPgA7jjLSDfDy0rDnh2kgPsUdiwa'
-const clientSecret = 'aNJfICP7fgr1cRt5JfBfGHrbTd4a'
+const clientID = 'IfLufdLinMxBrXfX3laC8sSi2pYa'
+const clientSecret = 'JpcpAGLsJZk3iWGkCpB8_Q7aON8a'
 
 const app = express()
-app.use(express.static(`./public`)) //${__dirname}
+app.use(express.static(`${__dirname}/public`))
+app.use(cookieParser())
+
 
 app.get('/oauth', (req, res) => {
     const requestToken = req.query.code
@@ -15,7 +18,7 @@ app.get('/oauth', (req, res) => {
     const base64data = buff.toString('base64');
 
     axios({
-        url: `https://wso2-gw.ua.pt/token?grant_type=authorization_code&code=${requestToken}&schema=openid&redirect_uri=localhost:3000`,
+        url: `https://wso2-gw.ua.pt/token?grant_type=authorization_code&code=${requestToken}&schema=openid&redirect_uri=http://localhost:3000/`,
         method: 'POST',
         headers: {
             Authorization: `Basic ${base64data}`,
@@ -38,6 +41,9 @@ app.get('/oauth', (req, res) => {
          * O user precisa de enviar sempre o access token, e se quiserem validar que ele está logged
          * precisam de perguntar pelo user info outra vez. Se falhar, têm de expirar o login
          */
+
+        res.cookie('cookieName', accessToken, options) // options is optional
+        res.send('')
 
         res.redirect('index.html');
     }).catch(console.error)
