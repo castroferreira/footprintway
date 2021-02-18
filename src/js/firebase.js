d47e44308bfa724d27c4cd488a91e47efee135d9
babrunhosa <babrunhosa@users.noreply.github.com>
2021-02-18 16:55:50 +0000

    erros

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,10 +1,24 @@
-var titulo = document.getElementById("titulo");
-var descricao = document.getElementById("descricao");
-var publicar = document.getElementById("publicar");
-var database = firebase.database();
+import * as firebase from "firebase/app";
+firebase.initializeApp ( {
+    apiKey: "AIzaSyAqgnHG58TJwd5ZaCbCSWwchPEWFlyThIE",
+    authDomain: "footprintway.firebaseapp.com",
+    databaseURL: "https://footprintway-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "footprintway",
+    storageBucket: "footprintway.appspot.com",
+    messagingSenderId: "705745529125",
+    appId: "1:705745529125:web:4872c408f265f5c77cd2cc",
+    measurementId: "G-QWL5XF7E6T"
+  });
+  
 
-function submitClick() {
-    window.alert("working");
+  console.log(firebase);
 
-   database.child("Dicas").set("poupa água");
-}
+  var database = firebase.database();
+  var ref= database.ref('dicas');
+
+  var data = {
+      dicadodia: "dicadodia5",
+      descricao: "Recolher agua"
+  }
+
+  ref.push(data);
