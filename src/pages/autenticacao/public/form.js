60ee368741a4de9ce5d1f697017ec64e607e2e19
danielantunes99 <dani.tunes05@gmail.com>
2021-02-24 17:39:03 +0000

    h

--- src/pages/autenticacao/public/form.js
+++ src/pages/autenticacao/public/form.js
@@ -9,4 +9,14 @@
 		alert("Signed Up");
 	}
 
-	
+
+	function signIn(){
+		
+		var email = document.getElementById("email");
+		var password = document.getElementById("password");
+		
+		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+		promise.catch(e => alert(e.message));	
+
+		alert("Signed In " + email.value);
+	}
