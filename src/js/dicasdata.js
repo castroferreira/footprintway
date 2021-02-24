934a7c13f53401802d447a81fa8eba8efc449842
babrunhosa <babrunhosa@users.noreply.github.com>
2021-02-24 19:37:28 +0000

    alteracoes2_barbara

--- src/js/dicasdata.js
+++ src/js/dicasdata.js
@@ -2,8 +2,6 @@ const titulo = document.getElementById("titulo");
 const descricao = document.getElementById("descricao");
 const publicar = document.getElementById("publicar");
 const apagar = document.getElementById("apagar");
-const titulo_dica = document.querySelector('#titulo_dica');
-const descricao_dica = document.querySelector('#descricao_dica');
 const database = firebase.database().ref("dicas");
 
 
@@ -11,25 +9,27 @@ const database = firebase.database().ref("dicas");
 publicar.addEventListener('click', (e) =>{
     e.preventDefault();
     database.child(titulo.value).set({
+        dica_titulo: titulo.value,
         dica_descricao: descricao.value
     });
 });
 
 database.on("child_added", snap => {
-    alert(snap.val());
-})
+    //alert(snap.val());
+    var nome = snap.child("dica_titulo").val();
+    var legenda = snap.child("dica_descricao").val();
 
-/*database.on("value", function(snapshot){
-    snapshot.forEach(function(dica){
-        document.querySelector('#titulo_dica').innerHTML += 
-        '<h5>' +  titulo.value()  + '</h5>'
-        
-    });
-});*/
+    $("#cardbody").append('<a href="#">'+'<button class="apagar" id="'+ apagar +'">'+
+    '<img src="../assets/img/icons/apagar.png" height="30px">'+'</button>'+'</a>'+
+    '<h5 class="card-title">' + nome + '</h5>'+
+    '<p class="card-text" id="descricao_dica">'+ legenda +'</p>');
+
+;})
+
 
 apagar.addEventListener('click', e => {
     e.preventDefault();
-    rootRef.child(titulo.value).remove().then(() => {
+    rootRef.child(dica_titulo).remove().then(() => {
 window.alert('Dica apagada!');
     })
 .catch.error(error => {
