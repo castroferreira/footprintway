c5e216c50485349a93a74acb121b412c939119cf
danielantunes99 <dani.tunes05@gmail.com>
2021-02-24 19:17:29 +0000

    atualização main

---
+++ src/pages/autenticacao/public/form.js
@@ -0,0 +1,23 @@
+    function signUp(){
+		
+		var email = document.getElementById("email");
+		var password = document.getElementById("password");
+		
+		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+		promise.catch(e => alert(e.message));
+	}
+
+
+	function signIn(){
+		
+		var email = document.getElementById("email");
+		var password = document.getElementById("password");
+		
+		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+		promise.catch(e => alert(e.message));	
+	}
+
+	function errData(err){​​​​​
+		console.log('Error');
+		console.log(err);
+	}​​​​​
