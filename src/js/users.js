e6b6e364f774f25de4be62046143538c229d9394
babrunhosa <babrunhosa@users.noreply.github.com>
2021-02-25 20:32:43 +0000

    alteracoes_ba

---
+++ src/js/users.js
@@ -0,0 +1,22 @@
+const username = document.getElementById("username");
+const idade = document.getElementById("quantity");
+const genero = document.getElementById("gender");
+const curso = document.getElementById("curso");
+const departamento = document.getElementById("departamento");
+const dados = document.getElementById("dados");
+
+const database = firebase.database().ref("users");
+
+
+
+dados.addEventListener('click', (e) =>{
+    e.preventDefault();
+    database.child(username.value).set({
+        username_dado: username.value,
+        idade_dado: idade.value,
+        genero_dado: genero.value,
+        curso_dado: curso.value,
+        departamento_dado: departamento.value
+    });
+});
+
