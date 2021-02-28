08ac48f48cdfc517d573cadfde8c5d23994bba76
Andreia Ferreira <castroferreira@ua.pt>
2021-02-28 00:22:19 +0000

    avatar picker a funcionar

--- src/js/avatar.js
+++ src/js/avatar.js
@@ -1,11 +1,36 @@
 $(document).ready(function (e) {
-    $('.avatar-items').click(function () {
-        $('#avatar-user').html($(this).html());
-        $('.dropdown-content').toggleClass('active');
+    $('.dropdown-content').toggleClass('active');
+    
+    $('.avatar-items').click(function() {
+      $('.avatar-user').html($(this).html());
+      $('.dropdown-content').toggleClass('active');
     });
-
-    $('.avatar-user').click(function () {
-        $('.dropdown-content').toggleClass('active');
+    
+    $('.avatar-user').click(function() {
+      $('.dropdown-content').toggleClass('active');                   
     });
+    
+  });
 
-});
+  
+
+  
+
+/* function pickAvatar(){
+		
+	var email = document.getElementById("email");
+	var password = document.getElementById("password");
+	
+	const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+	promise.catch(function() {
+		e => alert(e.message);
+		alert("Ocorreu um erro!")
+	}
+		);
+
+		promise.then(function() {
+			alert("Avatar escolhido com sucesso!")
+			window.location.replace("../pages/autenticacao/public/registo.html")
+		}
+			);
+} */
