f28b40b759ef408d15502fbfa159f627b84ce03b
danielantunes99 <dani.tunes05@gmail.com>
2021-03-01 21:59:28 +0000

    perfil tabela

--- src/js/pegadas.js
+++ src/js/pegadas.js
@@ -20,6 +20,10 @@ info.once('value', function (snapshot) {
     //alert(snap.val());
     snapshot.forEach(function(snap){
     var nome = snap.child("nome").val();
+    var idade_dados = snap.child("idade_dados").val();
+    var genero_dado = snap.child("genero_dado").val();
+    var curso_dados = snap.child("curso_dados").val();
+    var departamento_dado = snap.child("departamento_dado").val();
     
         $("#info_nome").append(
         '<div class="text-align-center">' +
@@ -27,6 +31,27 @@ info.once('value', function (snapshot) {
                 '<h2 class="subtitulo">'+ nome +'</h2>' +
         '</div>' 
         );
+
+        $("#table").append(
+            '<tbody>' +
+            '<tr> ' +
+                 '<th scope="row">Idade</th>' +
+                 '<td>'+ idade_dados +'</td>' +
+            '</tr>' +
+            '<tr>' +
+                 '<th scope="row">Género</th>' +
+                 '<td>'+ genero_dado +'</td>' +
+            '</tr>' +
+            '<tr>' +
+                 '<th scope="row">Curso</th>' +
+                 '<td>'+ curso_dados +'</td>' +
+            '</tr>' +
+            '<tr>' +
+                '<th scope="row">Departamento</th>' +
+                '<td>'+ departamento_dado +'</td>' +
+            '</tr>' +
+            '</tbody>' 
+            );
         });
     });
 
