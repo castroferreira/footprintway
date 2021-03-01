907a7663471e86ddf475de2e1c079eced3b8d89b
danielantunes99 <dani.tunes05@gmail.com>
2021-03-01 19:56:58 +0000

    atualização quase final

--- src/js/noticias.js
+++ src/js/noticias.js
@@ -1,5 +1,5 @@
  $.ajax({
-    url: 'https://api.unsplash.com/search/photos?query=ecological&client_id=Lpkvo7cc1CopuSpK_ez3HwhNIGhczR2Mwiw-K3I4x84',
+    url: 'https://api.unsplash.com/search/photos?query=ecology&client_id=Lpkvo7cc1CopuSpK_ez3HwhNIGhczR2Mwiw-K3I4x84',
     dataType: 'json',
     success: function (photos) {
       $.each(photos.results, function (index, photo) {
@@ -7,9 +7,12 @@
 
         $("#imagens2").append(
           '<div class="col-lg-6 col-sm-4 py-3">' +
-          ' <img class="img-fluid img-thumbnail zoom" src="' + photo.urls.regular + '" width="auto" height="auto">' +
+          '<img class="img-fluid img-thumbnail zoom" src="' + photo.urls.regular + '" width="auto" height="auto">' +
           '</div>'
         );
       });
     }
   });
+
+
+  
